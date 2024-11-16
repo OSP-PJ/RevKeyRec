aa603d478ff1b23c4e20405ad65e1977416b6bbe
20211938 <20211938@edu.hanbat.ac.kr>
2024-11-17 01:16:11 +0900

    코사인 유사도 추가

--- PythonWorkSpace/Back/link_to_DB.py
+++ PythonWorkSpace/Back/link_to_DB.py
@@ -1,13 +1,16 @@
 import firebase_admin
 from firebase_admin import credentials, db
 from dotenv import load_dotenv
-from fastapi import FastAPI,HTTPException
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import Any
 import os
 
-
-
+#코사인 유사도를 위한 호출과 변수명
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+from sentence_transformers import SentenceTransformer
+model = SentenceTransformer('jhgan/ko-sroberta-multitask')
 
 app = FastAPI() #FasAPI 인스턴스 생성
 load_dotenv()
@@ -40,6 +43,24 @@ class Process_of_DB: #firebase에 연결 및 데이터를 가져오는 클래스
         else:
             #raise -> 예외를 발생시키는 코드(의도적으로 예외 발생시키기 위함)
             raise HTTPException(status_code=404,detail='Data is not exist') #HTTP404오류 발생시키기_문서 데이터가 존재하지않는 경우
+
+# 새로 추가한 코사인 유사도 함수       
+    def get_similar_products(self, query: str, path: str = "product") :
+        data = get_data(path)
+        embeddings = [product['vector'] for product in data if product and 'vector' in product]
+        # 이 부분이 헷갈림 data 변수가 json을 어떻게 읽어오는지 모르겠음
+        query_emb = model.encode([query]) # 사용자가 입력한 단어에 대한 임베딩
+        
+        similarities = cosine_similarity(query_emb, embeddings)[0] # 각 항목과 코사인 유사도 실행
+        top_indices = np.argsort(similarities)[::-1][:5] #오름차순 후 상위 5개
+        
+        similar_products = []
+        for idx in top_indices: # 저장된 인덱스를 가지고 원래 위치의 항목을 불러옴
+            if data[idx] and 'name' in data[idx]:
+                similar_products.append({'name': data[idx]['name']}) # 원래 제품명을 반환
+        return similar_products #상위 5개에 대한 내용 리턴
+        
+        
         
         
 firebase_client = Process_of_DB(
@@ -63,3 +84,11 @@ def get_data(path: str):
     except HTTPException as e:
         return {"error":e.detail}
     
+#새로 추가한 부분
+@app.get("/get_similar_rpoducts")
+def get_similar_products(query: str):
+    try:
+        products_5 = firebase_client.get_similar_products(query)
+        return {"data":products_5}
+    except HTTPException as e:
+        return {"error": e.detail}
